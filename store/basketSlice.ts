8417ab576588fd1e38c8a2d7c408d043e742de16
bugattie <m.mohsinbarkaati@gmail.com>
2021-08-31 01:38:18 +0500

    All files added.

---
+++ store/basketSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { INITIAL_STATE } from './state';
+
+const basketSlice = createSlice({
+    name: 'basket',
+    initialState: INITIAL_STATE,
+    reducers: {
+        add: (state, action) => {
+            return state.map(item => {
+                if (item.id !== action.payload.id) return item;
+
+                return {
+                    ...item,
+                    added: true
+                }
+            });
+        },
+        remove: (state, action) => {
+            return state.map(item => {
+                if (item.id !== action.payload.id) return item;
+
+                return {
+                    ...item,
+                    added: false
+                }
+            });
+        }
+    }
+});
+
+export const { add, remove } = basketSlice.actions;
+export default basketSlice.reducer;
